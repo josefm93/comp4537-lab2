c8139db57b784576704df5f1685c048dc883aeed
Josef Murillo <jdmurillo93@gmail.com>
2023-01-17 11:16:46 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express(); 
+app.use(express.json()); // read JSON BODY
+app.use(express.urlencoded({ extended: true })); // read URL encoded body
+
+
+// app.get('/chatbot', (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+//   });
+
+app.post('/chatbot', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    const message = req.body.message;
+    const number = message.match(/\d+/);
+    if (number) {
+        fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+            res.json({
+                text: data
+            });
+        }).catch(error => {
+            res.json({
+                text: "Sorry, I couldn't find any information about that number."
+            });
+        });
+    } else {
+        res.json({
+            text: "I'm sorry, I didn't understand your question. Please provide a number for me to give you information about."
+        });
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
